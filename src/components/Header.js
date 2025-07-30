136e0ca7b90712a994a3e382f739708d25135b8f
Frankline Omollo <smithersfrank5@gmail.com>
2025-07-30 13:03:25 +0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,28 @@ export default function Header(){
     const [menuOpen, setMenuOpen]=useState(false);
 
     return(
-        <header className="header">
+      <>
+       {/*Header*/}
+       <header className="header">
+        <div className="logo">
+            <img src="/t9logo.jpg"alt="T9 Marketing"/>
+        </div>
+       </header>
 
-            {/*Logo*/}
-            <div className="logo">
-                <img src="/t9logo.jpg" alt="T9 Marketing"/>
-            </div>
+       {/*Floating Menu Button*/}
+       <div className="menu-toggle"onClick={()=>setMenuOpen(!menuOpen)}>
+         ☰
+       </div>
 
-            {/*Hamburger menu icon*/}
-            <div className="menu-toggle"onClick={()=>setMenuOpen(!menuOpen)}>
-                 ☰
-            </div>
+       {/*Sliding Navigation*/}
+       <nav className={menuOpen ? "open":""}>
+        <a href="#hero"onClick={()=>setMenuOpen(false)}>Home</a>
+        <a href="#services"onClick={()=>setMenuOpen(false)}>Services</a>
+        <a href="#contact"onClick={()=>setMenuOpen(false)}>Contact</a>
+       </nav>
 
-            {/*Navigation*/}
-            <nav className={menuOpen ?"open":""}>
-                {menuOpen && <div className="overlay"onClick={()=>setMenuOpen(false)}></div>}
-              <a href="#hero"onClick={()=>setMenuOpen(false)}>Home</a>
-              <a href="#services"onClick={()=>setMenuOpen(false)}>Services</a>
-              <a href="#contact"onClick={()=>setMenuOpen(false)}>Contact</a>
-            </nav>
-        </header>
+       {/*Background overlay*/}
+       {menuOpen && <div className="overlay"onClick={()=>setMenuOpen(false)}></div>}
+      </>
     );
 }
