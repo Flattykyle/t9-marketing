747713359760a12201e25782395b852d6d83b10c
Frankline Omollo <smithersfrank5@gmail.com>
2025-07-30 22:32:05 +0300

    update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import '../Hero.css';
+import React from "react";
+import "../Hero.css";
 
-export default function Hero(){
-    return(
-     <section className="hero" id="hero">
-        <div className="hero-content">
-            <h1>Grow Your Business with <span>T9 Marketing</span></h1>
-            <p>We help brands stand out online through strategy-driven Marketing
-                campaigns and creative solutions that deliver real results.
-            </p>
-            <a href="#contact"className="cta.btn">Get Started</a>
-        </div>        
-     </section>
+export default function Hero() {
+    return (
+        <section className="hero"id="hero">
+            <div className="hero-content">
+                <h1>Grow Your Business with<span>T9 Marketing</span></h1>
+                <p>We provide result-driven digital marketing services to help your business stand out online and reach more customers.</p>
+                <a href="#contact"className="cta-btn">Get Started</a>
+            </div>
+        </section>
     );
 }
